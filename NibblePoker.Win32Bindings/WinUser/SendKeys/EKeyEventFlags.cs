6a706fbdb85bc3ecfb3ac1602d0cfe13413acce6
Herwin Bozet <herwin.bozet@gmail.com>
2025-01-19 23:13:01 +0100

    Removed non-CC0 documentation Update EEventTypes.cs, EKeyEventFlags.cs, and EMouseEventFlags.cs

--- NibblePoker.Win32Bindings/WinUser/SendKeys/EKeyEventFlags.cs
+++ NibblePoker.Win32Bindings/WinUser/SendKeys/EKeyEventFlags.cs
@@ -4,37 +4,8 @@
 [Flags]
 public enum EKeyEventFlags : uint {
     // https://learn.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-keybdinput
-
-    /// <summary>
-    /// If specified, the wScan scan code consists of a sequence of two bytes, where the first byte has a value of 0xE0.
-    /// See <see href="https://learn.microsoft.com/en-us/windows/win32/inputdev/about-keyboard-input#extended-key-flag">Extended-Key Flag</see>
-    ///  for more info.
-    /// </summary>
-    /// <seealso cref="Structures.InputUnion"/>
-    /// <seealso cref="Structures.KEYBDINPUT"/>
     KEYEVENTF_EXTENDEDKEY = 0x0001,
-
-    /// <summary>
-    /// If specified, the key is being released.
-    /// If not specified, the key is being pressed.
-    /// </summary>
-    /// <seealso cref="Structures.InputUnion"/>
-    /// <seealso cref="Structures.KEYBDINPUT"/>
     KEYEVENTF_KEYUP = 0x0002,
-
-    /// <summary>
-    /// If specified, <see cref="Structures.KEYBDINPUT.wScan"/> identifies the key and <see cref="Structures.KEYBDINPUT.wVk"/> is ignored.
-    /// </summary>
-    /// <seealso cref="Structures.InputUnion"/>
-    /// <seealso cref="Structures.KEYBDINPUT"/>
     KEYEVENTF_SCANCODE = 0x0004,
-
-    /// <summary>
-    /// If specified, the system synthesizes a VK_PACKET keystroke.
-    /// The <see cref="Structures.KEYBDINPUT.wVk"/> parameter must be zero.
-    /// This flag can only be combined with the <see cref="KEYEVENTF_KEYUP"/> flag.
-    /// </summary>
-    /// <seealso cref="Structures.InputUnion"/>
-    /// <seealso cref="Structures.KEYBDINPUT"/>
     KEYEVENTF_UNICODE = 0x0008,
 }
--- NibblePoker.Win32Bindings/WinUser/SendKeys/EMouseEventFlags.cs
+++ NibblePoker.Win32Bindings/WinUser/SendKeys/EMouseEventFlags.cs
@@ -5,110 +5,18 @@
 public enum EMouseEventFlags : uint {
     // https://learn.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-MOUSEINPUT
 
-    /// <summary>
-    /// Movement occurred.
-    /// </summary>
-    /// <seealso cref="Structures.InputUnion"/>
-    /// <seealso cref="Structures.MOUSEINPUT"/>
     MOUSEEVENTF_MOVE = 0x0001,
-
-    /// <summary>
-    /// The left button was pressed.
-    /// </summary>
-    /// <seealso cref="Structures.InputUnion"/>
-    /// <seealso cref="Structures.MOUSEINPUT"/>
     MOUSEEVENTF_LEFTDOWN = 0x0002,
-
-    /// <summary>
-    /// The left button was released.
-    /// </summary>
-    /// <seealso cref="Structures.InputUnion"/>
-    /// <seealso cref="Structures.MOUSEINPUT"/>
     MOUSEEVENTF_LEFTUP = 0x0004,
-
-    /// <summary>
-    /// The right button was pressed.
-    /// </summary>
-    /// <seealso cref="Structures.InputUnion"/>
-    /// <seealso cref="Structures.MOUSEINPUT"/>
     MOUSEEVENTF_RIGHTDOWN = 0x0008,
-
-    /// <summary>
-    /// The right button was released.
-    /// </summary>
-    /// <seealso cref="Structures.InputUnion"/>
-    /// <seealso cref="Structures.MOUSEINPUT"/>
     MOUSEEVENTF_RIGHTUP = 0x0010,
-
-    /// <summary>
-    /// The middle button was pressed.
-    /// </summary>
-    /// <seealso cref="Structures.InputUnion"/>
-    /// <seealso cref="Structures.MOUSEINPUT"/>
     MOUSEEVENTF_MIDDLEDOWN = 0x0020,
-
-    /// <summary>
-    /// The middle button was released.
-    /// </summary>
-    /// <seealso cref="Structures.InputUnion"/>
-    /// <seealso cref="Structures.MOUSEINPUT"/>
     MOUSEEVENTF_MIDDLEUP = 0x0040,
-
-    /// <summary>
-    /// An X button was pressed.
-    /// </summary>
-    /// <seealso cref="Structures.InputUnion"/>
-    /// <seealso cref="Structures.MOUSEINPUT"/>
     MOUSEEVENTF_XDOWN = 0x0080,
-
-    /// <summary>
-    /// An X button was released.
-    /// </summary>
-    /// <seealso cref="Structures.InputUnion"/>
-    /// <seealso cref="Structures.MOUSEINPUT"/>
     MOUSEEVENTF_XUP = 0x0100,
-
-    /// <summary>
-    /// The wheel was moved, if the mouse has a wheel.
-    /// The amount of movement is specified in mouseData.
-    /// </summary>
-    /// <seealso cref="Structures.InputUnion"/>
-    /// <seealso cref="Structures.MOUSEINPUT"/>
     MOUSEEVENTF_WHEEL = 0x0800,
-
-    /// <summary>
-    /// The wheel was moved horizontally, if the mouse has a wheel.
-    /// The amount of movement is specified in mouseData.
-    /// </summary>
-    /// <remarks>Windows XP/2000: This value is not supported</remarks>
-    /// <seealso cref="Structures.InputUnion"/>
-    /// <seealso cref="Structures.MOUSEINPUT"/>
     MOUSEEVENTF_HWHEEL = 0x1000,
-
-    /// <summary>
-    /// The <see href="https://learn.microsoft.com/en-us/windows/win32/inputdev/wm-mousemove">WM_MOUSEMOVE</see>
-    ///   messages will not be coalesced.
-    /// The default behavior is to coalesce WM_MOUSEMOVE messages.
-    /// </summary>
-    /// <remarks>Windows XP/2000: This value is not supported</remarks>
-    /// <seealso cref="Structures.InputUnion"/>
-    /// <seealso cref="Structures.MOUSEINPUT"/>
     MOUSEEVENTF_MOVE_NOCOALESCE = 0x2000,
-
-    /// <summary>
-    /// Maps coordinates to the entire desktop.
-    /// Must be used with <see cref="MOUSEEVENTF_ABSOLUTE"/>.
-    /// </summary>
-    /// <seealso cref="Structures.InputUnion"/>
-    /// <seealso cref="Structures.MOUSEINPUT"/>
     MOUSEEVENTF_VIRTUALDESK = 0x4000,
-
-    /// <summary>
-    /// The dx and dy members contain normalized absolute coordinates.
-    /// If the flag is not set, `dx` and `dy` contain relative data (the change in position since the last reported position).
-    /// This flag can be set, or not set, regardless of what kind of mouse or other pointing device, if any, is connected to the system.
-    /// </summary>
-    /// <seealso cref="Structures.InputUnion"/>
-    /// <seealso cref="Structures.MOUSEINPUT"/>
     MOUSEEVENTF_ABSOLUTE = 0x8000,
 }
